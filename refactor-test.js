6b0720372537990b358a0262c1b4a3a421ea4bab
Alessandro Ledda <aleledda75@icloud.com>
2026-05-16 12:13:46 +0200

    snack 7

--- refactor-test.js
+++ refactor-test.js
@@ -32,10 +32,29 @@ const isPalindroma = (string) => {
     return string.trim() === stringaInversa
 }
 
+// SNACK 7
+const findPostId = (posts, id) => {
+    // se è valido l'id
+    if (isNaN(id)) {
+        throw new Error(`${id} non è un id`);
+    }
+    posts.forEach((post) => {
+        if (
+            post.id === undefined ||
+            post.title === undefined ||
+            post.slug === undefined
+        ) {
+            throw new Error("il primo argomento non si aspetta un array")
+        }
+    })
+    return posts.find(post => post.id === id) || null;
+}
+
 
 module.exports = {
     getInitials,
     createSlug,
     average,
-    isPalindroma
+    isPalindroma,
+    findPostId
 }
--- test-snack.test.js
+++ test-snack.test.js
@@ -1,5 +1,5 @@
 // import function from refactor
-const { getInitials, createSlug, average, isPalindroma } = require("./refactor-test.js");
+const { getInitials, createSlug, average, isPalindroma, findPostId } = require("./refactor-test.js");
 
 // SNACK 1
 test('la funzione "getInitials" restuisce la prima lettera del nomw completo ', () => {
@@ -41,3 +41,18 @@ test('la funzione "createSlug" lanci aun errore se l astringa non è valida', ()
     expect(() => createSlug("")).toThrow("titolo non valido");
     expect(() => createSlug(null)).toThrow("titolo non valido");
 });
+
+// SNACK 7
+
+// CREAZIONE OGGETTO 
+const posts = [
+    { id: 1, title: "caoitolo 1", slug: "capitolo-1" },
+    { id: 2, title: "caoitolo 2", slug: "capitolo-2" }
+]
+
+test("la funzione 'findPostId' restituisce il post corretto dato dall'array di post e l'id", () => {
+    expect(findPostId(posts, 2)).toEqual({ id: 2, title: "caoitolo 2", slug: "capitolo-2" });
+    expect(findPostId(posts, 7)).toBe(null);
+    expect(() => findPostId(posts, "casa")).toThrow("casa non è un id");
+    expect(() => findPostId([33, 45, 667], 2)).toThrow("il primo argomento non si aspetta un array");
+})
